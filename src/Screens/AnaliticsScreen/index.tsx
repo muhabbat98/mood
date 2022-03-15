156c8ef1c8144ba5fed16ce58491d54d26ce102a
muhabbat98 <55285047+muhabbat98@users.noreply.github.com>
2022-03-15 17:47:37 +0500

    useMemo

--- src/Screens/AnaliticsScreen/index.tsx
+++ src/Screens/AnaliticsScreen/index.tsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
-import {VictoryBar, VictoryChart, VictoryPie} from 'victory-native';
+import React, {useMemo} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {VictoryPie} from 'victory-native';
 import {useMood} from '../../Provider/MoodDate';
 
 const AnaliticScreen: React.FC = () => {
   const [mood] = useMood(true);
-  const [data, setData] = useState<any>([]);
-  let initialVal: any = [];
-  useEffect(() => {
+
+  const memoizedValue = useMemo(() => {
+    let initialVal: any = [];
     mood.reduce((_: any, current: any) => {
       if (
         !initialVal.find((i: any) => i?.description === current.description)
@@ -19,26 +19,18 @@ const AnaliticScreen: React.FC = () => {
         );
       }
     }, initialVal);
+    return initialVal;
   }, [mood]);
-  useEffect(() => {
-    // console.log( initialVal );
-    if (initialVal.length) {
-      setData(initialVal);
-    }
-  }, [initialVal]);
-  console.log(data);
+
   return (
     <View style={styles.container}>
-      {/* <VictoryChart width={350}>
-        <VictoryBar data={mood} x="description" y="emoji"></VictoryBar>
-      </VictoryChart> */}
       <VictoryPie
         colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']}
-        data={data.map((m: any) => ({
+        data={memoizedValue.map((m: any) => ({
           x: m.emoji,
           y: m.count,
         }))}
-        labelRadius={({innerRadius}: any) => innerRadius + 65}
+        labelRadius={({innerRadius}: any) => innerRadius + 75}
         style={{
           labels: {
             fontSize: 25,
@@ -52,7 +44,6 @@ const AnaliticScreen: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: 'red',
   },
 });
 
